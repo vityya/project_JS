eec5831beae450cb34479f9174702f8fa2ef35f5
Vitya <example@gmail.com>
2022-06-15 12:59:24 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,31 @@
+"use strict";
+//alert('Hello');
+
+//const result = confirm('Are you here?');
+
+// const answer = +prompt('Вам э 18 років?','18');
+
+
+// console.log(answer+5);
+// console.log(typeof(answer));
+
+// const answers = [];
+
+// answers[0] = prompt('Як ваше імя?','');
+// answers[1] = prompt('Як ваше фамілія?','');
+// answers[2] = prompt('Скільки вам років?','');
+
+// document.write(typeof())
+
+// const category = 'toys';
+
+// console.log(`http://someurl.com/${category}/5`);
+
+let incr = 10,
+    decr = 10;
+
+incr ++;
+decr --;
+
+console.log(incr++);
+console.log(decr);
